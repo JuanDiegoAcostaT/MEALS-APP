127f6e2b0a1f7b6b3b1326823b82d81ccca74429
JUAN.ACOSTA <juan.acosta@LATAM-WWFLJJ.local>
2023-01-30 11:49:03 -0500

    Category model typed

--- models/category.ts
+++ models/category.ts
@@ -1,13 +1,19 @@
-class Category {
-    id: any;
-    title: any;
-    color: any;
-    constructor(id: any, title: any, color: any) {
-      this.id = id;
-      this.title = title;
-      this.color = color;
-    }
+export type ICategory = {
+  id: string,
+  title: string,
+  color: string
+}
+
+class Category implements ICategory {
+
+  constructor(id: string, title: string, color: string) {
+    this.id = id;
+    this.title = title;
+    this.color = color;
   }
-  
-  export default Category;
-  
+  id: string;
+  title: string;
+  color: string;
+}
+
+export default Category;
