9d3f945f0f8b23c8ce3f2b53b042ace6ec601543
JUAN.ACOSTA <juan.acosta@LATAM-WWFLJJ.local>
2023-01-30 11:15:57 -0500

    models added

---
+++ models/category.ts
@@ -0,0 +1,13 @@
+class Category {
+    id: any;
+    title: any;
+    color: any;
+    constructor(id: any, title: any, color: any) {
+      this.id = id;
+      this.title = title;
+      this.color = color;
+    }
+  }
+  
+  export default Category;
+  
