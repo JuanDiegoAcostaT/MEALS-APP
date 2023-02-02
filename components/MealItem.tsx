8638093ed323f7f578fa958d3f8ea37a6fa245fa
JUAN.ACOSTA <juan.acosta@LATAM-WWFLJJ.local>
2023-02-01 20:55:58 -0500

    MealItem Styling improved

--- components/MealItem.tsx
+++ components/MealItem.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from "react"
-import { Text, View } from "react-native"
+import { Image, StyleSheet, Text, View } from "react-native"
 
 
 type IMealItem = {
@@ -17,7 +17,52 @@ function MealItem(
         affordability,
         complexity }:
         IMealItem): ReactElement {
+    return <View style={styles.mealCard} >
+        <Text style={styles.mealTitle} >{title}</Text>
+        <Image
+            style={styles.mealImage}
+            source={{ uri: image }} />
+        <View style={styles.mealDetails} >
+            <Text>Duration : {duration}</Text>
+            <Text>Affordability : {affordability}</Text>
+            <Text>Complexity : {complexity}</Text>
+        </View>
     </View>
 }
+
+
+const styles = StyleSheet.create({
+    mealImage: {
+        width: '100%',
+        height: 200,
+        resizeMode: 'cover',
+        borderRadius: 8
+    },
+
+    mealCard: {
+        borderRadius: 8,
+        backgroundColor: '#ccc',
+        padding : 16,
+        margin : 16,
+        shadowColor: 'black',
+        shadowOpacity: 0.25,
+        shadowOffset: { width: 2, height: 0  },
+        shadowRadius: 8,
+        elevation: 4,
+    },
+    mealTitle : {
+        fontSize : 24,
+        marginBottom : 6
+    },
+    mealDetails : {
+        borderRadius: 8,
+        backgroundColor: '#fff',
+        padding : 16,
+        marginTop : 12
+    }
+})
+
+
+
 
 export default MealItem
