cf33ca2b4932468af9f9f98639fd60dbe44ea497
JUAN.ACOSTA <juan.acosta@LATAM-WWFLJJ.local>
2023-02-09 18:43:03 -0500

    new Fav Screen added

---
+++ screens/FavoritesScreen.tsx
@@ -0,0 +1,8 @@
+import { ReactElement } from "react";
+import { Text, View } from "react-native";
+
+function FavoritesScreen () : ReactElement {
+    return <View><Text>Favs</Text></View>
+}
+
+export default FavoritesScreen
